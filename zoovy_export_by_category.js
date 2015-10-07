61901fbb1a3445ec7c89540f92ef74028d58eb5f
Jay Mendoza <jay.mendoza@bigcommerce.com>
2015-10-07 11:21:19 -0500

    add initial working version

---
+++ zoovy_export_by_category.js
@@ -0,0 +1,17 @@
+var main = {
+    start: 23,
+    end: 63
+}
+
+function runMacro() {
+    macro = "VERSION BUILD=8920312 RECORDER=FX\n";
+
+    for (var i = main.start; i < main.end; i++) {
+        macro += "TAG POS=1 TYPE=LABEL FORM=NAME:productExportToolForm ATTR=data-key:" + i + " CONTENT=YES\n";
+    }
+        
+    imPlayCode(macro);
+    iimClose();
+}
+
+runMacro();
